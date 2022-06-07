94ca186fa18da50092a18fbebd3d5baf56213cfc
robertsonj48 <robertsonj48@gmail.com>
2022-06-07 18:40:22 -0400

    c

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ MongoClient.connect(mongoConnectionString, {
 
 
     const PORT = process.env.PORT || 3001;
-    app.set(PORT)
+    app.listen(PORT)
     console.log(`Server running on port ${PORT}`)
 
 })
